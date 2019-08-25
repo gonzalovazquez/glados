c4e84e25e9ab9beff646f867bc744021f49baa91
gonzalovazquez <gonzalovazquez010@gmail.com>
2019-08-25 12:08:15 -0400

    feat: 🎸 update to latest react version

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import React, { Component } from 'react';
-import logo from './glados.gif';
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import logo from './images/glados.gif';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>GLaDOS</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <React.Fragment>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <CssBaseline />
+      <Container maxWidth="sm">
+      <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Paper>
+              Test
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper>
+              Test
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
 }
 
 export default App;
